b332a8cfcd1a6ea04abfa607027385ed4480ef0a
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2019-11-27 08:06:17 +0545

    Using two different profiles to access to different services.

--- chapter2/src/main/kotlin/com/microservices/chapter2/Chapter2Application.kt
+++ chapter2/src/main/kotlin/com/microservices/chapter2/Chapter2Application.kt
@@ -2,12 +2,23 @@ package com.microservices.chapter2
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
 
 @SpringBootApplication
-class Chapter2Application
+class Chapter2Application {
+    // if a bean hasn't been explicitly annotated, we have to provide them here
+    @Bean
+	@ConditionalOnExpression("#{'\${service.type}'=='simple'}")
+    fun exampleService(): ServiceInterface = ExampleService()
+
+	@Bean
+	@ConditionalOnExpression("#{'\${service.type}'=='advanced'}")
+	fun advancedService(): ServiceInterface = AdvancedService()
+}
 
 @Controller
 class FirstController {
@@ -23,9 +34,9 @@ fun main(args: Array<String>) {
     runApplication<Chapter2Application>(*args)
 }
 
-@RestController
-class SecondController(val exampleService: ExampleService) {
-
-    @GetMapping(path = ["/second/{name}"])
-    fun secondHello(@PathVariable name: String) = exampleService.getHello(name)
-}
+//@RestController
+//class SecondController(val exampleService: ExampleService) {
+//
+//    @GetMapping(path = ["/second/{name}"])
+//    fun secondHello(@PathVariable name: String) = exampleService.getHello(name)
+//}
--- chapter2/src/main/kotlin/com/microservices/chapter2/ExampleService.kt
+++ chapter2/src/main/kotlin/com/microservices/chapter2/ExampleService.kt
@@ -3,7 +3,7 @@ package com.microservices.chapter2
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
-@Service
+// @Service // if this is commented, we have to provide the service bean explicitly in Main class
 class ExampleService : ServiceInterface {
     @Value(value = "\${service.message.text}")
     private lateinit var text: String
