9fbe07ea915735189987da42f9470f3856b5fd2e
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2019-11-28 08:31:22 +0545

    Trialling with null object values and excluding them in the response.

--- chapter3/src/main/kotlin/com/microservices/kotlin/chapter3/model/Customer.kt
+++ chapter3/src/main/kotlin/com/microservices/kotlin/chapter3/model/Customer.kt
@@ -1,3 +1,9 @@
 package com.microservices.kotlin.chapter3.model
 
-data class Customer(var id: Int = 0, var name: String = "")
+import com.fasterxml.jackson.annotation.JsonInclude
+
+// this annotation is not needed here, if we include a global configuration in the application.yml file
+// @JsonInclude(JsonInclude.Include.NON_NULL)
+data class Customer(var id: Int = 0, var name: String = "", var telephone: Telephone? = null)
+
+data class Telephone(var countryCode: String = "", var telephoneNumber: String = "")
--- chapter3/src/main/kotlin/com/microservices/kotlin/chapter3/service/CustomerServiceImpl.kt
+++ chapter3/src/main/kotlin/com/microservices/kotlin/chapter3/service/CustomerServiceImpl.kt
@@ -1,6 +1,7 @@
 package com.microservices.kotlin.chapter3.service
 
 import com.microservices.kotlin.chapter3.model.Customer
+import com.microservices.kotlin.chapter3.model.Telephone
 import org.springframework.stereotype.Service
 import java.util.concurrent.ConcurrentHashMap
 
@@ -8,9 +9,10 @@ import java.util.concurrent.ConcurrentHashMap
 class CustomerServiceImpl : CustomerService {
     companion object {
         val initialCustomers = arrayOf(
-                Customer(1, "Puppy"),
-                Customer(2, "Chubbu"),
-                Customer(3, "Buppu")
+                Customer(1, "Puppy", Telephone("977", "9841122030")),
+                Customer(2, "Chubbu", Telephone("1", "7312345679")),
+                Customer(3, "Buppu", Telephone("100", "123456789")),
+                Customer(14, "Dhuppy", null)
         )
     }
 
