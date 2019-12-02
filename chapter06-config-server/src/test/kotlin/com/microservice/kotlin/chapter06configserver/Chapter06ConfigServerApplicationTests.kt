0d83b469bda785b90b4e5060de65415726ea2961
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2019-12-02 13:27:41 +0545

    Init commit of microservice project

---
+++ chapter06-config-server/src/test/kotlin/com/microservice/kotlin/chapter06configserver/Chapter06ConfigServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.microservice.kotlin.chapter06configserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Chapter06ConfigServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
