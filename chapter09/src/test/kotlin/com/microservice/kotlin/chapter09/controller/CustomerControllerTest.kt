504ce7f20fa352ff0965cd5d6cd831e994e9a085
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2019-12-07 20:19:06 +0545

    Added code to test Controller using MockMvc.

---
+++ chapter09/src/test/kotlin/com/microservice/kotlin/chapter09/controller/CustomerControllerTest.kt
@@ -0,0 +1,16 @@
+package com.microservice.kotlin.chapter09.controller
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class CustomerControllerTest {
+
+    @Test
+    fun getCustomer() {
+    }
+
+    @Test
+    fun getAllCustomers() {
+    }
+}
