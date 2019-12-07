28878cbbe97f6a532edd213fc4507f00486ca3a5
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2019-12-07 21:01:41 +0545

    adding chapter06 discovery server code.

---
+++ chapter06-discovery-server/src/test/kotlin/com/microservice/kotlin/chapter06discoveryserver/Chapter06DiscoveryServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.microservice.kotlin.chapter06discoveryserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Chapter06DiscoveryServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
