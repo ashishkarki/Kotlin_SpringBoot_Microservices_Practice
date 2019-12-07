f9e59ad0f0671326cae90c66a888d4c59f923d62
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2019-12-07 20:57:21 +0545

    committing chapter08 code.

--- chapter08/src/main/kotlin/com/microservice/kotlin/chapter08/controller/HelloController.kt
+++ chapter08/src/main/kotlin/com/microservice/kotlin/chapter08/controller/HelloController.kt
@@ -1,4 +1,20 @@
 package com.microservice.kotlin.chapter08.controller
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.*
+import java.util.concurrent.atomic.AtomicInteger
+
+@RestController
 class HelloController {
+
+    private val id: String = UUID.randomUUID().toString()
+
+    companion object {
+        val total: AtomicInteger = AtomicInteger()
+    }
+
+    @GetMapping("/hello")
+    fun hello() = "Hello I am $id and I have been called  ${total.incrementAndGet()} times"
+
 }
