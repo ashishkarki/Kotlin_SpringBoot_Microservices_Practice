afe9b9751a8f0635cff4bd13356e4b7e6d8cb479
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2019-12-08 20:13:33 +0545

    Using more Kluent based testing notations.

--- chapter09/src/test/kotlin/com/microservice/kotlin/chapter09/controller/CustomerControllerTest.kt
+++ chapter09/src/test/kotlin/com/microservice/kotlin/chapter09/controller/CustomerControllerTest.kt
@@ -12,10 +12,31 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.ResultActions
+import org.springframework.test.web.servlet.ResultHandler
+import org.springframework.test.web.servlet.ResultMatcher
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.JsonPathResultMatchers
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+class WithKeyword {
+    infix fun `json path`(expression: String) = jsonPath("\$" + expression)
+}
+
+val With = WithKeyword()
+
+class ThatKeyword {
+    infix fun `status is http`(value: Int) = status().`is`(value)
+}
+
+val That = ThatKeyword()
+
+infix fun JsonPathResultMatchers.`that the value is`(value: Any) = this.value(value)
+infix fun ResultActions.`and expect`(matcher: ResultMatcher) = this.andExpect(matcher)
+infix fun ResultActions.`and then do`(handler: ResultHandler) = this.andDo(handler)
+infix fun MockMvc.`do a get request to`(uri: String) = this.perform(get(uri))
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
@@ -42,11 +63,17 @@ class CustomerControllerTest {
         When calling customerService.getCustomer(1) `it returns`
                 Customer(1, mockName)
 
-        mockMvc.perform(get("/customer/1"))
-                .andExpect(status().isOk)
-                .andExpect(jsonPath("\$.id").value(1))
-                .andExpect(jsonPath("\$.name").value(mockName))
-                .andDo(print())
+//        mockMvc.perform(get("/customer/1"))
+//                .andExpect(status().isOk)
+//                .andExpect(jsonPath("\$.id").value(1))
+//                .andExpect(jsonPath("\$.name").value(mockName))
+//                .andDo(print())
+        (mockMvc `do a get request to` "/customer/1"
+                `and expect` (That `status is http` 200)
+                `and expect` (With `json path` ".id" `that the value is` 1)
+                `and expect` (With `json path` ".name" `that the value is` mockName)
+                ) `and then do` print()
+
 
 //        then(customerService).should(times(1)).getCustomer(1)
 //        then(customerService).shouldHaveNoMoreInteractions()
@@ -65,14 +92,24 @@ class CustomerControllerTest {
         When calling customerService.getAllCustomers() `it returns`
                 listOf(Customer(1, "test1"), Customer(2, "test2"))
 
-        mockMvc.perform(get("/customers"))
-                .andExpect(status().isOk)
-                .andExpect(jsonPath("\$").isArray)
-                .andExpect(jsonPath("\$[0].id").value(1))
-                .andExpect(jsonPath("\$[0].name").value("test1"))
-                .andExpect(jsonPath("\$[1].id").value(2))
-                .andExpect(jsonPath("\$[1].name").value("test2"))
-                .andDo(print())
+//        mockMvc.perform(get("/customers"))
+//                .andExpect(status().isOk)
+//                .andExpect(jsonPath("\$").isArray)
+//                .andExpect(jsonPath("\$[0].id").value(1))
+//                .andExpect(jsonPath("\$[0].name").value("test1"))
+//                .andExpect(jsonPath("\$[1].id").value(2))
+//                .andExpect(jsonPath("\$[1].name").value("test2"))
+//                .andDo(print())
+        (mockMvc `do a get request to` "/customers"
+                `and expect` (That `status is http` 200)
+                `and expect` (With `json path` "[0].id" `that the value is` 1)
+                `and expect` (With `json path` "[0].name" `that the value is`
+                "test1")
+                `and expect` (With `json path` "[1].id" `that the value is` 2)
+                `and expect` (With `json path` "[1].name" `that the value is`
+                "test2")
+                ) `and then do` print()
+
 
         // also, use Kluent assertion here
 //        then(customerService).should(times(1)).getAllCustomers()
