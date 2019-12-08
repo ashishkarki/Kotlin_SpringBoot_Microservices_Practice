d912f60cbe3a8affa47d490a6f7f5b3b89d8f8fc
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2019-12-08 09:47:19 +0545

    Added Kluent based assertions to Service Unit test.

--- chapter09/src/test/kotlin/com/microservice/kotlin/chapter09/service/CustomerServiceTest.kt
+++ chapter09/src/test/kotlin/com/microservice/kotlin/chapter09/service/CustomerServiceTest.kt
@@ -1,7 +1,6 @@
 package com.microservice.kotlin.chapter09.service
 
-import org.junit.Assert
-import org.junit.Assert.*
+import org.amshove.kluent.*
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -16,15 +15,24 @@ class CustomerServiceTest {
     lateinit var customerService: CustomerService
 
     @Test
-    fun `we should get a customer with a valid name`() {
+    fun `we should get a customer with a valid Id`() {
         val customer = customerService.getCustomer(1)
-        assertNotNull(customer)
-        assertEquals(customer?.name, "Kotlin")
+
+        // assertNotNull(customer) // use Kluent assertion instead of this; more expressive
+        // customer.shouldNotBeNull()
+        customer.`should not be null`() //can use words in Kluent
+
+        // assertEquals(customer?.name, "Kotlin")
+        customer?.name `should be` "Kotlin"
     }
 
     @Test
     fun `we should get all customers`() {
         val customers = customerService.getAllCustomers()
-        assertTrue(customers.size == 3)
+
+        // assertTrue(customers.size == 3) // use Kluent assertions
+        customers.size `should equal to` 3
+        customers.size `should be greater than` 0
+        customers.size `should be less or equal to` 3
     }
 }
