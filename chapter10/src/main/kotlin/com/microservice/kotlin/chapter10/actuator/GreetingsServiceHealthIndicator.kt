008845c4c58330a227c2c4bce4938ab6ff5027ee
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2019-12-09 21:05:17 +0545

    Added configurations to application yml to enable health details to be displayed.

--- chapter10/src/main/kotlin/com/microservice/kotlin/chapter10/actuator/GreetingsServiceHealthIndicator.kt
+++ chapter10/src/main/kotlin/com/microservice/kotlin/chapter10/actuator/GreetingsServiceHealthIndicator.kt
@@ -1,2 +1,28 @@
 package com.microservice.kotlin.chapter10.actuator
 
+import com.microservice.kotlin.chapter10.service.GreetingsService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.actuate.health.AbstractHealthIndicator
+import org.springframework.boot.actuate.health.Health
+import org.springframework.stereotype.Component
+import java.lang.Exception
+
+@Component
+class GreetingsServiceHealthIndicator : AbstractHealthIndicator() {
+
+    @Autowired
+    private lateinit var greetingsService: GreetingsService
+
+    override fun doHealthCheck(builder: Health.Builder?) {
+        val lastMessage = try {
+            val message = greetingsService.getGreeting()
+            builder?.up()
+            message
+        } catch (e: Exception) {
+            builder?.down()
+            "ERROR: $e"
+        }
+
+        builder?.withDetail("lastMessage: ", lastMessage)
+    }
+}
